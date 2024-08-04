ac7a48e2b1e07224b256ecee2f712b9bab80a190
Michael2258 <thanh2t405@yahoo.com>
2024-08-05 01:35:28 +0700

    config to build

---
+++ src/components/AnalogClock/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AnalogClock = () => {
+  return <div>This is analog clock</div>;
+};
+
+export default AnalogClock;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "promodoro-components",
+      fileName: "promodoro-components",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+    },
+  },
+});
