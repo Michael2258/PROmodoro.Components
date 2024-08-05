7272f384723a6609da518395e577f35e147af454
Michael2258 <thanh2t405@yahoo.com>
2024-08-06 01:01:35 +0700

    publish to npm registry

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.js"),
       name: "promodoro-components",
-      fileName: "promodoro-components",
+      fileName: "index",
     },
     rollupOptions: {
       external: "./src",
